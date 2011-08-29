6c1a599753f221dea874ac017dc405003611f267
Sam Pohlenz <sam@sampohlenz.com>
2011-08-29 10:28:19 +0930

    Add Sprockets::Asset monkey-patch and tests

--- lib/fingerprintless-assets.rb
+++ lib/fingerprintless-assets.rb
@@ -2,6 +2,7 @@ require "sprockets"
 require 'sprockets/base'
 
 require "fingerprintless_assets/monkey_patches/configuration"
+require "fingerprintless_assets/monkey_patches/asset"
 require "fingerprintless_assets/monkey_patches/environment"
 require "fingerprintless_assets/monkey_patches/index"
 require "fingerprintless_assets/monkey_patches/utils"
