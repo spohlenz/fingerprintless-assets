32631c06b5cdde10080b15a22fa29109b53ba97d
Sam Pohlenz <sam@sampohlenz.com>
2011-09-05 13:07:01 +0930

    Deprecate gem

--- lib/fingerprintless-assets.rb
+++
@@ -1,9 +0,0 @@
-require "sprockets"
-require 'sprockets/base'
-
-require "fingerprintless_assets/monkey_patches/configuration"
-require "fingerprintless_assets/monkey_patches/asset"
-require "fingerprintless_assets/monkey_patches/environment"
-require "fingerprintless_assets/monkey_patches/index"
-
-require "fingerprintless_assets/railtie" if defined?(Rails)
